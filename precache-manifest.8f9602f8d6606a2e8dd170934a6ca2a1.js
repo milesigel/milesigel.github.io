91cc8c185d58a1d2797fdcf8bacb59c58327b236
Miles Sigel <mgs6@rice.edu>
2023-04-14 15:26:33 -0500

    Updates

---
+++ precache-manifest.8f9602f8d6606a2e8dd170934a6ca2a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47e649869782db21b90e",
+    "url": "/pub_purity_test/static/css/main.93781620.chunk.css"
+  },
+  {
+    "revision": "47e649869782db21b90e",
+    "url": "/pub_purity_test/static/js/main.4d5077e7.chunk.js"
+  },
+  {
+    "revision": "cdc13f29fb64fad6a360",
+    "url": "/pub_purity_test/static/js/runtime~main.64d26161.js"
+  },
+  {
+    "revision": "e443e264bec0db3ddb01",
+    "url": "/pub_purity_test/static/js/2.6684072d.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/pub_purity_test/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "2c973845b0ab525a7a4c7dbc8dd51a93",
+    "url": "/pub_purity_test/index.html"
+  }
+];
